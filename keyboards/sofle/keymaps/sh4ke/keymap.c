4483f04455c00a498706c50333ac9a1f0db81e48
Michael Wittig <michael.wittig@posteo.de>
2023-04-23 17:33:25 +0200

    adding gaming stage

--- keyboards/sofle/keymaps/sh4ke/keymap.c
+++ keyboards/sofle/keymaps/sh4ke/keymap.c
@@ -4,6 +4,7 @@ enum sofle_layers {
     /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
     _QWERTY,
     _NUMPAD,
+    _GAMING,
     _LOWER,
     _RAISE,
     _ADJUST,
@@ -12,6 +13,7 @@ enum sofle_layers {
 enum custom_keycodes {
     KC_QWERTY = SAFE_RANGE,
     KC_NUMPAD,
+    KC_GAMING,
     KC_LOWER,
     KC_RAISE,
     KC_ADJUST,
@@ -70,6 +72,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LCTRL, KC_Y,   KC_X,    KC_C,    KC_V,    KC_B, _______,   _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PENT,   _______,
                _______, _______, _______, _______, _______,      _______,  _______, _______, _______, _______
 ),
+/*
+ * GAMING
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  DEL |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |  Up  |   O  |   P  |  ~   |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |LShift|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  | Left | Down | Right|   ;  |  '   |
+ * |------+------+------+------+------+------| MUTE  |    |       |------+------+------+------+------+------|
+ * | LCTR |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  | RCTR |
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            | LGUI | LAlt |LShift|LOWER | /Space  /       \Enter \  |RAISE |RShift| RAlt | RGUI |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            `----------------------------------'           '------''---------------------------'
+ */
+
+[_GAMING] = LAYOUT(
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6, KC_7,    KC_8,     KC_9,    KC_0,     KC_DEL,
+  KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y, KC_U,    KC_UP,    KC_O,    KC_P,     KC_TILDE,
+  KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H, KC_LEFT, KC_DOWN,  KC_RGHT, KC_SCLN,  KC_QUOT,
+  KC_LCTRL, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,   KC_MUTE, KC_N, KC_M,    KC_COMMA, KC_DOT,  KC_SLSH,  KC_RCTRL,
+                 KC_LGUI,KC_LALT,KC_LSFT, KC_LOWER,  KC_SPC,      KC_ENT, KC_RAISE, KC_RSFT, KC_RALT, KC_RGUI
+),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
@@ -116,7 +141,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | RESET|      |QWERTY|NUMPAD|      |      |                    |      |      |      |      |      |      |
+ * | RESET|      |QWERTY|NUMPAD|GAMING|      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------.    ,-------|      | VOLDO| MUTE | VOLUP|      |      |
  * |------+------+------+------+------+------|  MSTP |    |       |------+------+------+------+------+------|
@@ -128,7 +153,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
   [_ADJUST] = LAYOUT(
   XXXXXXX , XXXXXXX, XXXXXXX ,  XXXXXXX , XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  RESET   , XXXXXXX, KC_QWERTY, KC_NUMPAD,XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  RESET   , XXXXXXX, KC_QWERTY,KC_NUMPAD,KC_GAMING,XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   XXXXXXX , XXXXXXX, XXXXXXX,   XXXXXXX,  XXXXXXX, XXXXXXX,                     XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX,
   XXXXXXX , XXXXXXX, XXXXXXX,   XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
                    _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
@@ -221,6 +246,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case KC_NUMPAD:
             if (record->event.pressed) {
                 set_single_persistent_default_layer(_NUMPAD);
+            }
+            return false;
+        case KC_GAMING:
+            if (record->event.pressed) {
+                set_single_persistent_default_layer(_GAMING);
             }
             return false;
         case KC_LOWER:
